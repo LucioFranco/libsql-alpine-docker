f27b36795df5b20c6a6b1c1a3306c37309bc8075
Lucio Franco <luciofranco14@gmail.com>
2024-09-09 11:17:31 -0400

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:22-alpine as base
+
+RUN apk add --no-cache libc6-compat
+WORKDIR /home/node/app
+
+COPY . .
+
+RUN npm i
+
+RUN npx tsc 
+
+CMD ["node", "src/index.js"]
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { createClient } from "@libsql/client";
+
+export const db = createClient({
+  url: "file:local.db",
+});
+
+await db.batch(
+    [
+        "CREATE TABLE IF NOT EXISTS users (email TEXT)",
+        "INSERT INTO users (email) VALUES ('alice@example.com')",
+        "INSERT INTO users (email) VALUES ('bob@example.com')",
+    ],
+    "write",
+);
+
+const rs = await db.execute("SELECT * FROM users");
+console.log(rs);
+
